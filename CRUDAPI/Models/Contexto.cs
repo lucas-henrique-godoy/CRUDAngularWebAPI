6fba784032e2534e17de2ba367c6678cd355a610
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-11 01:06:37 -0300

    Criei a classe contexto do banco de dados,  e a connection string

--- CRUDAPI/Models/Contexto.cs
+++ CRUDAPI/Models/Contexto.cs
@@ -9,5 +9,10 @@ namespace CRUDAPI.Models
     public class Contexto : DbContext    
     {
         public DbSet<Pessoa> Pessoas { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {
+            
+        }
     }
 }
