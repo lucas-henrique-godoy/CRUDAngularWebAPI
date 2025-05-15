2ebacb67e690448fb86887e64f7429cfea14f31f
lucas-henrique-godoy <lucashenrique1507@outlook.com>
2025-05-14 22:27:51 -0300

    criei outro controller para treinar

---
+++ CRUDAPI/Controllers/PessoaController2.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using CRUDAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDAPI.Controllers
+{
+     [ApiController]
+     [Route("api/[controller]")]
+    public class PessoaController2 : ControllerBase
+    {
+       private readonly Contexto _contexto;
+       public PessoaController2(Contexto contexto)
+       {
+            _contexto = contexto;
+       }
+       
+       [HttpGet]
+       public async Task<ActionResult<IEnumerable<Pessoa>>> PegarTodosAsync()
+       {
+          return await _contexto.Pessoas.ToListAsync();
+       }  
+
+       [HttpGet("{pessoaId}")]
+       public async Task<ActionResult>
+    }
+}
