87a7aacc665787f8ad5685b8cbbfdf2e59c46eab
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-18 21:23:34 -0300

    importando alguns módulos para que  oa angular funcione e instalando algumas dependências.

--- AppAngular/src/app/app.module.ts
+++ AppAngular/src/app/app.module.ts
@@ -1,8 +1,12 @@
+import { PessoasService } from './pessoas.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
