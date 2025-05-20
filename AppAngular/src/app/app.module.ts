88288b87c55e61716d2033be71126854055edafc
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-19 22:28:46 -0300

    .

--- AppAngular/src/app/app.module.ts
+++ AppAngular/src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { CommonModule } from '@angular/commom';
 import { PessoasService } from './pessoas.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {CommonModule} from '@angular/common';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
