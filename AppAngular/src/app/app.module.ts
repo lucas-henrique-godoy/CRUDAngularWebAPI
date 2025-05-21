b5f6a280b31b3762c31865db83f339d7d55bd1a9
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-20 22:31:52 -0300

    .

--- AppAngular/src/app/app.module.ts
+++ AppAngular/src/app/app.module.ts
@@ -1,23 +1,21 @@
-import { CommonModule } from '@angular/commom';
 import { PessoasService } from './pessoas.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,    // já inclui CommonModule internamente
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    // PessoasService aqui só se não usar @Injectable({ providedIn: 'root' }) no serviço
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
