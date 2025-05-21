bc3d14604063b4d92fe1658cf43a52db59adf9d7
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-20 22:48:17 -0300

    Update app.module.ts

--- AppAngular/src/app/app.module.ts
+++ AppAngular/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { PessoasService } from './pessoas.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'; //Para usar ngIf e ngFor
+import {HttpClientModule} from '@angular/common/http'; 
+import { ReactiveFormsModule } from '@angular/forms'; //Uma das maneiras para usar formulários no angular
+import {ModalModule} from  'ngx-bootstrap/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,10 +15,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,    // já inclui CommonModule internamente
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot()
   ],
   providers: [
     // PessoasService aqui só se não usar @Injectable({ providedIn: 'root' }) no serviço
+    HttpClientModule,
+    PessoasService
   ],
   bootstrap: [AppComponent]
 })
