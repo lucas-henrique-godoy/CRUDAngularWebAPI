c278e949093187c44800cd97ba7dc3af9dcaf1d9
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-21 23:38:29 -0300

    Instalei as versões compatíveis do ngx-bootstrap

--- AppAngular/src/app/app.component.ts
+++ AppAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AppAngular';
+
+  constructor (private modalService: BsModalService){
+    console.log('ModalService', modalService);
+  }
 }
