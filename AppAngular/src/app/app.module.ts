dda5a9f5e28c4123a1cb15354b5185da84624344
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-24 19:09:23 -0300

    Update app.module.ts

--- AppAngular/src/app/app.module.ts
+++ AppAngular/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { PessoasService } from './pessoas.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//import { CommonModule } from '@angular/common'; //Para usar ngIf e ngFor
-import {HttpClientModule} from '@angular/common/http'; 
+import { CommonModule } from '@angular/common'; //Para usar ngIf e ngFor
+import {HttpClientModule} from '@angular/common/http'; // Para requisições http
 import { ReactiveFormsModule } from '@angular/forms'; //Uma das maneiras para usar formulários no angular
-import {ModalModule} from  'ngx-bootstrap/modal';
+import {ModalModule} from  'ngx-bootstrap/modal'; //Para mostrar avisos quando for excluir algum dado
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
