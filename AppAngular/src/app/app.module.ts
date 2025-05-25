0e8de368c162489d08e4904d12fa79aaa04ee152
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-25 14:23:02 -0300

    Registrando módulos que serão utilizados pela aplicação Angular em app.module.ts

--- AppAngular/src/app/app.module.ts
+++ AppAngular/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { PessoasService } from './pessoas.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common'; //Para usar ngIf e ngFor
-import {HttpClientModule} from '@angular/common/http'; // Para requisições http
-import { ReactiveFormsModule } from '@angular/forms'; //Uma das maneiras para usar formulários no angular
-import {ModalModule} from  'ngx-bootstrap/modal'; //Para mostrar avisos quando for excluir algum dado
+import { CommonModule } from '@angular/common'; //Para usar ngIf e ngFor.
+import {HttpClientModule} from '@angular/common/http'; // Para requisições http.
+import { ReactiveFormsModule } from '@angular/forms'; //Uma das maneiras para usar formulários no angular.
+import {ModalModule} from  'ngx-bootstrap/modal'; //Para mostrar avisos quando for excluir algum dado.
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,14 +14,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,    // já inclui CommonModule internamente
+    BrowserModule,    // já inclui CommonModule internamente.
     AppRoutingModule,
+    CommonModule,
     HttpClientModule,
     ReactiveFormsModule,
-    ModalModule.forRoot()
+    ModalModule.forRoot()//Usando forRoot para registrar o modal  para a aplicaçao inteira.
   ],
   providers: [
-    // PessoasService aqui só se não usar @Injectable({ providedIn: 'root' }) no serviço
+    // PessoasService aqui só se não usar @Injectable({ providedIn: 'root' }) no serviço.
     PessoasService
   ],
   bootstrap: [AppComponent]
