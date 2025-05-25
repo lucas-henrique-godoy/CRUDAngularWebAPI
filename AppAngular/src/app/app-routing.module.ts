f455e207c99067a69959261cdeaba45c67363530
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-25 20:50:54 -0300

    Registrei a rota do componente de pessoas no app-routing.modue.ts

--- AppAngular/src/app/app-routing.module.ts
+++ AppAngular/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { PessoasComponent } from './components/pessoas/pessoas.component';
 
 const routes: Routes = [{
-  path: 'pessoas', component: PessoasComponent
+  path: 'pessoas', component: PessoasComponent  // Aqui é onde são registradas as rotas de cada componente que criamos. O path é a nossa url, ou seja nosso servidor subiu no http://localhost:4200/, se eu digito: http://localhost:4200/pessoas, vai cair no nosso componente de pessoas.  
 }];
 
 @NgModule({
