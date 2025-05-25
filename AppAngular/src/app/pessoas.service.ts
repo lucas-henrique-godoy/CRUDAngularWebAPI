ae2a5e732feaf3269e20d28d565f544f33c9a8d8
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-25 15:47:50 -0300

    Update pessoas.service.ts

--- AppAngular/src/app/pessoas.service.ts
+++ AppAngular/src/app/pessoas.service.ts
@@ -23,8 +23,16 @@ export class PessoasService {
 
   PegarPeloId(pessoaId: number): Observable<Pessoa>{    //Retorna uma pessoa pelo id, como sera apenas 1 pessoa, não precisa de um array ou lista.
     const apiUrl = `${this.url}/${pessoaId}`;           //Configurar a url pois será enviado  um id // const apiUrl = `${this.url}/${pessoaId}`; --> Esta modificando a nossa url original para acrescentar um id, ou seja a nossa url  que foi feita la em cima, + o id da pessoa.
-    return this.http.get<Pessoa>(apiUrl);
+    return this.http.get<Pessoa>(apiUrl);               //Retorna o que sera usado para fazer requisiçoes http(this.http) --> Pegar uma pessoa(get<Pessoa>(apiUrl)) --> Passar a url modiicada que foi colcoada na variavel apiUrl.
   }
 
+  SalvarPessa(pessoa: Pessoa): Observable<any>{ // SalvarPessoa insere uma nova pessoa. --> Observable<any> Usamos "any" porque ainda não sabemos o tipo da resposta da API
+    return this.http.post<Pessoa>(this.url, pessoa, httpOptions); // Retornar o que será usado para fazer requisições http(this.http) -->  Envia dados do tipo pessoa (return this.http.post<Pessoa>) --> Depois envia para o servidor a url(this.url) --> osddos que estão na variavel pessoa --> Envia tambem as opções do cabeçalho da nossa requisição http que esta na variavel(httpOptions).
+  }
+
+  AtualizarPessoa(pessoa: Pessoa): Observable<any>{     // AtualizarPessoa atualiza os dados da pessoa --> Observable<any> Usamos any pois não iremos receber um tipo conhecido
+    return this.http.put<Pessoa>(this.url, pessoa, httpOptions);  //
+  }
+    
   
 }
