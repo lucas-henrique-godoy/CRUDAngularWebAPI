adb316e795804dcbcb6c2045cce5893da75da8aa
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-25 21:00:11 -0300

    Update pessoas.component.ts

--- AppAngular/src/app/components/pessoas/pessoas.component.ts
+++ AppAngular/src/app/components/pessoas/pessoas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-pessoas',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pessoas.component.css']
 })
 export class PessoasComponent implements OnInit {
+  formulario: any; //Variável que representa nosso formulário.
+  tituloFormluario: string; //Variável que representa o titulo do formulario.
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { //Onde nossos componentes são inicializados.
+
+    this.formulario = new FormGroup({ //FormGroup agrupa os FormControls que são os inputs.
+      nome: new FormControl(null),
+      sobrenome: new FormControl(null),  //Essa área serve para dar nomes para os nossos inputs(FormControls).
+      profissao: new FormControl(null)
+    })
   }
 
 }
