6865ad93dcc9b04c1371153ae336386ba6435f92
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-28 23:47:19 -0300

    criando o formulario

--- cadastro-pessoas/cadastro-pessoas/src/app/pessoa-form/pessoa-form.component.ts
+++ cadastro-pessoas/cadastro-pessoas/src/app/pessoa-form/pessoa-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,EventEmitter,Input, OnInit,Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Pessoa } from '../Model/pessoa.model';
 
 @Component({
   selector: 'app-pessoa-form',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pessoa-form.component.css']
 })
 export class PessoaFormComponent implements OnInit {
+  @Input() pessoa: Pessoa | null = null;
+  @Output() salvar = new EventEmitter<Pessoa>();
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      id: [this.pessoa?.id],
+      nome: [this.pessoa?.nome || '',Validators.required],
+      emsil: [this.pessoa?.email || '', [Validators.required, Validators.email]],
+      idade: [this.pessoa?.idade ?? '', [Validators.min(0)]] 
+    });
   }
 
+  onSubmit() {
+    if(this.form.valid) {
+      this.salvar.emit(this.form.value)
+      this.form.reset();
+    }
+  }
 }
