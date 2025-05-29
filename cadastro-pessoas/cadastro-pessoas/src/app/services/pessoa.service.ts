23323d60fc36037dad4940fa71f5f70c6c88d26b
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-28 23:28:53 -0300

    Update pessoa.service.ts

--- cadastro-pessoas/cadastro-pessoas/src/app/services/pessoa.service.ts
+++ cadastro-pessoas/cadastro-pessoas/src/app/services/pessoa.service.ts
@@ -15,4 +15,39 @@ export class PessoaService {
       this.pessoas = JSON.parse(dadosSalvos);
     }
    }
+
+   getAll(): Pessoa[] {
+    return this.pessoas;
+   }
+
+   add(pessoa: Pessoa): void {
+    pessoa.id = this.generateId();
+    this.pessoas.push(pessoa);
+    this.save();
+   }
+
+   update(pessoa: Pessoa): void {
+    const index = this.pessoas.findIndex(p => p.id === pessoa.id);
+    if(index !== 1){
+      this.pessoas[index] = pessoa;
+      this.save();
+    }
+   }
+
+   delete(id: number): void {
+    this.pessoas = this.pessoas.filter(p => p.id !== id);
+    this.save();
+   }
+
+   private save(): void {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.pessoas));
+   }
+
+   private generateId(): number {
+    return this.pessoas.length > 0 ? Math.max(...this.pessoas.map(p => p.id)) + 1 : 1;
+   }
+
+   getById(id: number): Pessoa | undefined {
+    return this.pessoas.find(p => p.id === id);
+   }
 }
